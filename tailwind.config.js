23156c4f2d838223bc880fa6b174e6b2540980a4
Turaconokiki <turaconjoy@gmail.com>
2023-08-26 11:05:44 +0100

    Completed work

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    sm: '640px',
+    md: '760px',
+    lg: '1024px',
+    xl: '1020px',
+    '2xl': '1536px'
+  },
+  plugins: [],
+}
+
